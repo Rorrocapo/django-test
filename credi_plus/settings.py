4dcb6c647996f3aa8812dd8f271c07cdbfd62c93
Rorrocapo <darkess_erick@hotmail.com>
2021-07-12 10:54:02 -0500

    update

--- credi_plus/settings.py
+++ credi_plus/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-di%%+!fv5y8ui1*g#6a7+%i)95+dor8%!(-j$q6a7d+#kreg@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
